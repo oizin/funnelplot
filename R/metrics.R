54c5082e7f96b6a038ab50d165fa54f9b3dc08f8
Oisin Fitzgerald <o.fitzgerald@unsw.edu.au>
2018-08-30 12:09:46 +1000

    addition of casemix adjustment evaluation metrics

---
+++ R/metrics.R
@@ -0,0 +1,144 @@
+#' classify a probability
+#'
+#' @param prob
+#' @param cutoff
+#'
+classify <- function(prob, cutoff = 0.5) {
+  pred <- rep(0, length(prob))
+  pred[prob >= cutoff] <- 1
+  pred
+}
+
+#' labels data like a confusion matrix
+#'
+#' @param pred
+#' @param truth
+#'
+confusion <- function(pred,truth) {
+  labelled <- rep("TN",length(pred))
+  labelled[(pred == truth) & (truth == 1)] <- "TP"
+  labelled[(pred != truth) & (truth == 0)] <- "FP"
+  labelled[(pred != truth) & (truth == 1)] <- "FN"
+  labelled
+}
+
+#' accuracy
+#'
+#' @param pred
+#' @param truth
+#'
+accuracy <- function(pred,truth) {
+  sum(pred == truth)/length(pred)
+}
+
+#' error rate
+#'
+#' @param pred
+#' @param truth
+#'
+err <- function(pred,truth) {
+  1 - accuracy(pred,truth)
+}
+
+#' recall, sensitivity, true positive rate
+#'
+#' @param pred
+#' @param truth
+#'
+tpr <- function(pred,truth) {
+  tp <- sum((pred == truth) & (truth == 1))
+  fn <- sum((pred != truth) & (truth == 1))
+  tp / (tp + fn)
+}
+
+#' specificity
+#'
+#' @param pred
+#' @param truth
+#'
+sp <- function(pred,truth) {
+  tn <- sum((pred == truth) & (truth == 0))
+  fp <- sum((pred != truth) & (truth == 0))
+  tn / (tn + fp)
+}
+
+#' fpr
+#'
+#' @param pred
+#' @param truth
+#'
+fpr <- function(pred,truth) {
+  fp <- sum((pred != truth) & (truth == 0))
+  tn <- sum((pred == truth) & (truth == 0))
+  fp / (tn + fp)
+}
+
+#' precision, positive predictive value
+#'
+#' @param pred
+#' @param truth
+#'
+ppv <- function(pred,truth) {
+  tp <- sum((pred == truth) & (truth == 1))
+  fp <- sum((pred != truth) & (truth == 0))
+  tp / (tp + fp)
+}
+
+#' Receiver operating characteristic curve
+#'
+#'
+roc <- function(prob,truth,length_out=100) {
+  cutoffs <- seq(0,1,length.out=length_out)
+  res <- data.frame(cutoffs, tpr = NA, fpr = NA)
+  for (i in 1:length_out) {
+    pred <- classify(prob,cutoff = cutoffs[i])
+    res$tpr[i] <- tpr(pred,truth)
+    res$fpr[i] <- fpr(pred,truth)
+  }
+  res
+}
+
+#' Area under the receiver operating characteristic curve
+#'
+auc_roc <- function(prob,truth,length_out=100) {
+  roc_df <- roc(prob,truth,length_out=length_out)
+  out <- numeric(length_out-1)
+  for (i in 1:length(out)) {
+    y <- rev(roc_df$tpr)
+    x <- rev(roc_df$fpr)
+    out[i] <- ((y[i] + y[i+1])/2)*(x[i+1]-x[i])
+  }
+  sum(out,na.rm=TRUE)
+}
+
+pr <- function(prob,truth,length_out=100) {
+  cutoffs <- seq(0,1,length.out=length_out)
+  res <- data.frame(cutoffs, ppv = NA, tpr = NA)
+  for (i in 1:length_out) {
+    pred <- classify(prob,cutoff = cutoffs[i])
+    res$ppv[i] <- ppv(pred,truth)
+    res$tpr[i] <- tpr(pred,truth)
+  }
+  res
+}
+
+# ttt <- pr(predict_out,true_out)
+# plot(ttt$tpr,ttt$ppv,type="l", lty=2,col="blue")
+
+
+#' Area under the precision recall curve
+#'
+#'
+auc_pr <- function(prob,truth,length_out=100) {
+  pr_df <- pr(prob,truth,length_out=length_out)
+  out <- numeric(length_out-1)
+  for (i in 1:length(out)) {
+    y <- rev(pr_df$ppv)
+    x <- rev(pr_df$tpr)
+    out[i] <- ((y[i] + y[i+1])/2)*(x[i+1]-x[i])
+  }
+  sum(out,na.rm=TRUE)
+}
+
+# auc_pr(predict_out,true_out)
+
